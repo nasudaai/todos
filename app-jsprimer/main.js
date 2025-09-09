9e176fcba0492cb880801461b5baea5cf11d6b0d
nasuda <nasuda908@gmail.com>
2025-09-10 01:03:41 +0900

    add function "main.js"

--- app-jsprimer/main.js
+++ app-jsprimer/main.js
@@ -2,3 +2,9 @@ import { App } from "./src/app.js";
 
 const app = new App();
 console.log("main");
+
+const formElement = document.querySelector("#js-form");
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("test");
+})
